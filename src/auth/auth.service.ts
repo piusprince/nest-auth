24e78a59de34ddd1c495450e2829d03bef7a99d6
Pius Prince <45078705+piusprince@users.noreply.github.com>
2023-06-29 15:07:40 +0000

    Create sign up and login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hashedPassword = this.hashedData(dto.password);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hashedPassword,
+        name: dto.name,
+      },
+    });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRefreshToken(newUser.id, tokens.refreshToken);
+
+    return tokens;
+  }
+
+  async loginLocal({ email, password }: AuthDto): Promise<Tokens> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user)
+      throw new ForbiddenException('Access Denied!. This user does not exist');
+
+    const isPasswordValid = await bcrypt.compare(password, user.hashedPassword);
+
+    if (!isPasswordValid)
+      throw new ForbiddenException(
+        'Access Denied. You entered a wrong password',
+      );
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRefreshToken(user.id, tokens.refreshToken);
+
+    return tokens;
+  }
+
+  logout() {}
+
+  refreshTokens() {}
+
+  async updateRefreshToken(userId: number, refreshToken: string) {
+    const hash = await this.hashedData(refreshToken);
+
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRefreshToken: hash,
+      },
+    });
+  }
 
   hashedData(password: string) {
     return bcrypt.hashSync(password, 10);
   }
 
-  signupLocal(dto: AuthDto): Promise<Tokens> {
-    const hashedPassword = this.hashedData(dto.password);
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        { sub: userId, email },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        { sub: userId, email },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
 
-    const newUser = this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hashedPassword,
-      },
-    });
+    return {
+      accessToken: at,
+      refreshToken: rt,
+    };
   }
-
-  loginLocal() {}
-
-  logout() {}
-
-  refreshTokens() {}
 }
